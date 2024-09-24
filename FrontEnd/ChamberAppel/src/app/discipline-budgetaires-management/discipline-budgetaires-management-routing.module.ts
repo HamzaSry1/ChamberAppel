be4a980cf6594e4a3f02cd12e77c66cf923ce1c4
HamzaSry1 <84987127+HamzaSry1@users.noreply.github.com>
2024-09-25 00:15:17 +0100

    save changes

--- FrontEnd/ChamberAppel/src/app/discipline-budgetaires-management/discipline-budgetaires-management-routing.module.ts
+++ FrontEnd/ChamberAppel/src/app/discipline-budgetaires-management/discipline-budgetaires-management-routing.module.ts
@@ -1,7 +1,29 @@
+import { DisciplineBudgetaireListComponent } from './discipline-budgetaire-list/discipline-budgetaire-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component : DisciplineBudgetaireListComponent,
+  },
+  {
+    path:"ajouter",
+    component : DisciplineBudgetaireListComponent,
+  },
+  {
+    path:"supprimer",
+    component : DisciplineBudgetaireListComponent,
+  },
+  {
+    path:"modifier",
+    component : DisciplineBudgetaireListComponent,
+  },
+  {
+    path:"detail/:id",
+    component : DisciplineBudgetaireListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
