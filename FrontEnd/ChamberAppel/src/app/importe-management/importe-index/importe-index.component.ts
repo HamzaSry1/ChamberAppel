ae23e6feb0f1f0f180e7c3215ea65766c743de8b
HamzaSry1 <84987127+HamzaSry1@users.noreply.github.com>
2024-09-29 00:42:16 +0100

    Add importation component

--- FrontEnd/ChamberAppel/src/app/importe-management/importe-index/importe-index.component.ts
+++ FrontEnd/ChamberAppel/src/app/importe-management/importe-index/importe-index.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonStyle } from 'src/app/shared/button-style';
-
+import Stepper from 'bs-stepper';
 @Component({
   selector: 'app-importe-index',
   templateUrl: './importe-index.component.html',
   styleUrls: ['./importe-index.component.scss']
 })
-export class ImporteIndexComponent {
+export class ImporteIndexComponent implements OnInit {
+
+  ngOnInit(): void {
+    const stepperElement = document.querySelector('#stepper1');
+    if (stepperElement !== null) {
+      this.stepper = new Stepper(stepperElement, {
+        linear: false,
+        animation: true,
+      });
+    }
+  }
 
   public ReturnButtonStyle = ButtonStyle.secondary;
   public ExempleButtonStyle = ButtonStyle.success;
+  public stepper!: Stepper;
 
+
+  Next() {
+    this.stepper.next();
+  }
+
+  Previous() {
+    this.stepper.previous();
+  }
 }
